1f97151b56562c74d663709b297ec91331c955ea
Muhammad Naufal <naufal@paper.id>
2025-08-01 01:01:45 +0700

    feat: add user profile component with conditional rendering and unit tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CounterComponent, LoginFormComponent],
+  imports: [RouterOutlet, CounterComponent, LoginFormComponent, UserProfileComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-testing-demo';
