518667b9eb58133ef0746c8aa8b8b96d67492dd1
Muhammad Naufal <naufal@paper.id>
2025-07-31 23:16:55 +0700

    feat: implement login form component with validation and unit tests using Jest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CounterComponent, LoginFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
